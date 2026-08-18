f9855caff535b7b4d7af7c19774d3e568a6421bb
jchainsonsol <jchainsonsol@gmail.com>
2026-08-18 18:52:56 -0400

    Define OBS scene routing for W3BC rundown

---
+++ obs-scene-map.js
@@ -0,0 +1,16 @@
+window.W3BC_OBS_SCENE_MAP = {
+  byName: {
+    "Open": "JCHAINS",
+    "Markets Now": "SHOW PROGRAM",
+    "News Desk": "SHOW PROGRAM",
+    "AI + Technology": "SHOW PROGRAM",
+    "Sports": "SHOW PROGRAM",
+    "Betting Desk": "SHOW PROGRAM",
+    "The Trenches": "SHOW PROGRAM",
+    "X Space": "SHOW PROGRAM",
+    "Interview": "JCHAINS & GUEST",
+    "Close": "JCHAINS"
+  },
+  byKind: {
+    markets: "SHOW PROGRAM",
+    news
